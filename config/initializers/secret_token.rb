aa55a533f71e5612ccacdf65dd3b6799bb1101b9
stefanielim <stefanie.lim04@gmail.com>
2014-09-18 14:45:49 +0100

    initial commit; adds rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheerupsApp::Application.config.secret_token = '4ec8c454f031be91060caf63622fb368e7bf32a569e476da37e78dd47c3ef9bdf0c98902b21df7207e094399a4cdc1dbeff6ba103e9b23c6f9e9de78d494fc75'
