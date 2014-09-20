2db85b7c691c7522f68dae49bbc49e5c077da95c
aduddella <aduddella@gmail.com>
2014-09-20 11:23:12 +0100

    adds devise to the existing user model with index action for Users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :user_name , :role, :status
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+  :name, :user_name , :role, :status
 
   validates :name, presence: true, allow_blank: false
   validates :user_name, presence: true, allow_blank: false
