3faffd4a677a3163b37d7cbf00c135a3956b9583
stefanielim <stefanie.lim04@gmail.com>
2014-09-22 12:05:50 +0100

    modifies seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.delete_all
 User.create(name: "anand", user_name: "aduddella", role: "admin", status: "active",
   email: "aduddella@gmail.com", password: "password", password_confirmation: "password")
 User.create(name: "stefanie lim", user_name: "stef", role: "admin", status: "active",
   email: "stefanie.lim04@gmail.com", password: "password", password_confirmation: "password")
 User.create(name: "kate montgomery", user_name: "kate", role: "admin", status: "active", email: "katharine.montgomery@gmail.com", password: "password", password_confirmation: "password")
 
-
+Cheerup.delete_all
 Cheerup.create(content: "Feeling great about starting our cheerups app! :D", user_id: 2, created_at: "2014-09-20 17:56:01", updated_at: "2014-09-20 17:56:01")
 Cheerup.create(content: "Setting up this cheerup model is sooo thrilling!", user_id: 2, created_at: "2014-09-20 18:02:10", updated_at: "2014-09-20 18:02:10")
 
