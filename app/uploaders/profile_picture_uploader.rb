ef893423ec943a87994b193ea24773ff41de008c
katemont <katharine.montgomery@gmail.com>
2014-09-22 21:07:43 +0100

    adds rmagick to uploader

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -3,7 +3,7 @@
 class ProfilePictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -32,10 +32,14 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fill => [100, 100]
+  end
 
+  version :detail do
+    process :resize_to_fit => [400, 400]
+  end
+  
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
