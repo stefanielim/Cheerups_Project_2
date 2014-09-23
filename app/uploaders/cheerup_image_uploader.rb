1e89c3ba0a92109bdb5024a8741c9fd8bc76df8b
katemont <katharine.montgomery@gmail.com>
2014-09-23 11:24:01 +0100

    updates uploader to include Rmagick

--- app/uploaders/cheerup_image_uploader.rb
+++ app/uploaders/cheerup_image_uploader.rb
@@ -3,7 +3,7 @@
 class CheerupImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -32,10 +32,13 @@ class CheerupImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fill => [150, 150]
+  end
 
+  version :detail do
+    process :resize_to_fit => [250, 250]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
