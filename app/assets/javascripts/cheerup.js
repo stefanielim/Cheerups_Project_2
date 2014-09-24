3226678e1b38374d6054de5181bb98f0bd0b8575
stefanielim <stefanie.lim04@gmail.com>
2014-09-24 12:14:20 +0100

    adds some animation to new cheerup form slide down and up

--- app/assets/javascripts/cheerup.js
+++ app/assets/javascripts/cheerup.js
@@ -2,7 +2,9 @@
 $(function(){
 
   $('#new_cheerup_title').on('click', function(){
-    $('#new_cheerup_form').toggleClass('hidden');
+    $('#new_cheerup_form').slideToggle(400, function(){
+      $('#new_cheerup_form').toggleClass('hidden');
+    });
   });
 
 });
