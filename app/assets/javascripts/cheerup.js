99664630fb2cbad54d2993977e3aeac64eb60506
aduddella <aduddella@gmail.com>
2014-09-24 10:03:32 +0100

    adds the ajax method in cheerup.js to upvote and downvote. Updates the voting record but js console has a 404 error

---
+++ app/assets/javascripts/cheerup.js
@@ -0,0 +1,67 @@
+
+$().ready(function(e){
+
+  console.log("document ready");
+  $("a[id *= downvote],a[id ^= upvote]").on('click',function(event) { 
+
+    //event.preventDefault();
+    var cheerupVoteAction = event.currentTarget.id.split('_')[0];  
+    var cheerup_id = event.currentTarget.id.split('_')[1];  
+    var url = " ";
+    var direction = " ";
+    if (cheerupVoteAction == "upvote") {
+      url = "/cheerups/"+ cheerup_id + "/vote/up";
+      direction = "up";
+    }
+    else {
+      url = "/cheerups/"+ cheerup_id + "/vote/down";
+      direction = "down";
+    }
+
+    console.log(event);
+    console.log(event.currentTarget.id);
+    console.log(cheerup_id);
+    console.log(cheerupVoteAction);
+    console.log(url);
+
+    $.ajax({
+      type: "PUT",
+      dataType: 'json',
+      url: url+".json",
+      success: function(){
+
+      var id =  cheerupVoteAction+ "_" +cheerup_id;
+  
+      if (cheerupVoteAction == "upvote") {
+
+        var idOfAttributeToBeSet = "prominence_"+id;
+        console.log('p#'+idOfAttributeToBeSet);
+        $('p#'+idOfAttributeToBeSet).text("Prominence 1");
+        $("'p#'+id").text("Upvotes 1");                
+
+      }
+      else {
+      $('p#id').text("Downvotes 1");                        
+      }
+
+      console.log("successful post");
+     }
+
+    });
+
+    return false;
+  });
+
+
+  // function to create a cheerup using the ajax method 
+
+  $('#new_cheerup').on('submit',function(event){
+
+    event.preventDefault();
+
+    console.log($("#inner-editor").text());
+    console.log("form submitted");
+
+  });
+});
+
