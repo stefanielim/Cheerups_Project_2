6ad32545a494ee7a4de9fd8d336aad2c7420fe19
katemont <katharine.montgomery@gmail.com>
2014-09-25 11:46:32 +0100

    adds confirmable to user model and personalises mailer text

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
+  :confirmable,
   :recoverable, :rememberable, :trackable, :validatable,
-  :omniauthable, :omniauth_providers => [:facebook]
+  :omniauthable, :omniauth_providers => [:facebook] 
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
